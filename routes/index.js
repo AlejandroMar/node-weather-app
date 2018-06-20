0c004de007c0ba2306d47930b99351c7ff4029a1
AlejandroMar <motungo@hotmail.com>
2018-06-20 16:00:23 +0200

    add icons to the app

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,8 @@ router.post('/', function(req, res, next) {
   console.log(req.body.city);
   axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&units=imperial&appid=${apiKey}`)
     .then(data => {
-      console.log(data.data.main);
-      res.render('index', { data: data.data.main})
+      console.log("http://openweathermap.org/img/w/" + data.data.weather[0].icon +".png");
+      res.render('index', { data: data.data.main, icon: `http://openweathermap.org/img/w/${data.data.weather[0].icon}.png`})
     
     })
 });
